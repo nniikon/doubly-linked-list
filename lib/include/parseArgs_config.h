be1cffe2f1c5cecb057abac12b0bdadef54645ef
nniikon <kolya.kasparov@gmail.com>
2024-03-31 21:30:09 +0300

    remove console args

--- lib/include/parseArgs_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSE_ARGS_CONFIG_H
-#define PARSE_ARGS_CONFIG_H
-
-const char* const PARSE_OPT_HELP = "-h";
-const char* const PARSE_INCORRECT_INPUT = "Error! Incorrect input. Write %s %s for help.\n";
-
-#endif
